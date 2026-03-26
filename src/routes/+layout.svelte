57269b82997837b8b6539f15faf1cd8128f6c2a6
eschencode <linuseschenbach@gmail.com>
2026-03-26 21:14:22 +0100

    improve for ipad os

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.ico';
-
 	let { children } = $props();
-
-	import { onMount } from 'svelte';
-  import { fileHandlerStore } from '$lib/stores/fileHandler';
-  
-  onMount(() => {
-    console.log('🚀 App initialized - starting file handler');
-    fileHandlerStore.init();
-    
-    // Cleanup when app closes (browser tab closed)
-    return () => {
-      console.log('👋 App closing - stopping file handler');
-      fileHandlerStore.stopChecking();
-    };
-  });
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
